7b79ff789caf2b6c7fdfd90a576f507d56c07bc3
Lei Zhao <zhao434@usc.edu>
2017-04-05 08:00:13 -0700

    2017-04-05 08:00:05 auto check in

---
+++ test/typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/body-parser/index.d.ts" />
+/// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/nunjucks/index.d.ts" />
