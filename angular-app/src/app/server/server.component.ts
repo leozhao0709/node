348438b6e763668453a5a7ba1e75a165bd5e8a37
Lei Zhao <zhao434@usc.edu>
2017-04-14 08:00:07 -0700

    2017-04-14 08:00:04 auto check in

--- angular-app/src/app/server/server.component.ts
+++ angular-app/src/app/server/server.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  // styleUrls: ['./name.component.css']
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
+  serverId = 10;
+  serverStatus = 'offline';
+  constructor() { }
+
+  ngOnInit() { }
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
