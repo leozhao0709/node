9f47ee1aebe2308958a63bc5d3384601bc481897
leozhao0709 <zhao434@usc.edu>
2017-10-19 20:17:55 -0700

    restructure

---
+++ react/udemy/indecision-app/src/index.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+const app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two'],
+}
+
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+ReactDom.render(template, document.getElementById('app'));
--- webpackLearn/src/js/sub.js
+++
@@ -1,7 +0,0 @@
-// module.exports = {
-//     add: (a, b)=>{
-//         console.log(a - b);
-//     }
-// }
-
-console.log("我是b");
