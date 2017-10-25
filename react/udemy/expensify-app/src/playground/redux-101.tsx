5f5024e9c6df4a591be45d8483ee708c119f2c56
Lei Zhao <zhao434@usc.edu>
2017-10-25 08:00:06 -0700

    2017-10-25 08:00:03 auto check in

---
+++ react/udemy/expensify-app/src/playground/redux-101.tsx
@@ -0,0 +1,98 @@
+import { createStore } from 'redux';
+
+enum TypeKeys {
+  INCREMENT = 'INCREMENT',
+  DECREMENT = 'DECREMENT',
+  RESET = 'RESET',
+  SET = 'SET'
+}
+
+interface IncrementAction {
+  type: TypeKeys.INCREMENT;
+  incrementBy: number;
+}
+
+interface DecrementAction {
+  type: TypeKeys.DECREMENT;
+  decrementBy: number;
+}
+
+interface ResetAction {
+  type: TypeKeys.RESET;
+}
+
+interface SetAction {
+  type: TypeKeys.SET;
+  to: number;
+}
+
+type ActionTypes =
+  | IncrementAction
+  | DecrementAction
+  | ResetAction
+  | SetAction;
+
+interface ICountState {
+  count: number;
+}
+
+const store = createStore((state: ICountState = { count: 0 }, action: ActionTypes): ICountState => {
+  switch (action.type) {
+    case TypeKeys.INCREMENT:
+      return {
+        count: state.count + action.incrementBy
+      };
+    case TypeKeys.DECREMENT:
+      return {
+        count: state.count - action.decrementBy
+      };
+    case TypeKeys.RESET:
+      return {
+        count: 0,
+      };
+    case TypeKeys.SET:
+      return {
+        count: action.to,
+      };
+    default:
+      return {
+        count: state.count
+      };
+  }
+});
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+const incrementCount = (incrementBy: number = 1): IncrementAction => ({
+  type: TypeKeys.INCREMENT,
+  incrementBy
+});
+
+const decrementCount = (decrementBy: number = 1): DecrementAction => ({
+  type: TypeKeys.DECREMENT,
+  decrementBy
+});
+
+const resetCount = (): ResetAction => ({
+  type: TypeKeys.RESET
+});
+
+const setCount = (to: number): SetAction => ({
+  type: TypeKeys.SET,
+  to
+});
+
+store.dispatch(incrementCount());
+
+store.dispatch(incrementCount(5));
+
+// unsubscribe();
+
+
+store.dispatch(resetCount());
+store.dispatch(setCount(90));
+store.dispatch(decrementCount());
+store.dispatch(decrementCount(5));
+
