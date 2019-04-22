9f550763daf01b92d5dd65a05638afe4943f7d9f
Lei Zhao <zhao434@usc.edu>
2019-04-21 21:48:26 -0700

    2019-04-21 21:48:16 auto check in

--- udemy_max/nest-learn/src/main.ts
+++ udemy_max/nest-learn/src/main.ts
@@ -1,8 +1,20 @@
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as path from 'path';
+import * as nunjucks from 'nunjucks';
+import { HttpExceptionFilter } from './filters/httpException/http-exception.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(path.resolve(__dirname, 'public'));
+  nunjucks.configure(path.resolve(__dirname, 'views'), {
+    autoescape: true,
+    express: app,
+  });
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+  await app.listen(5000);
 }
 bootstrap();
