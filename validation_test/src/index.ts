41320dcb57ffc52e75c5d7c75e6e06411a903176
Lei Zhao <lei.zhao@coupa.com>
2021-06-07 08:54:43 -0700

    add by auto checkIn

--- validation_test/src/index.ts
+++ validation_test/src/index.ts
@@ -1,22 +1,21 @@
-import * as yup from 'yup';
+import { z } from 'zod';
 
-type Person = {
-  firstName: string;
-};
-
-const schema: yup.SchemaOf<Person> = yup.object().shape({
-  firstName: yup.string().required('this is required').min(5, 'at least 5').max(10, 'max with 10'),
+const schema = z.object({
+  firstName: z.string().min(5, 'at least 5').max(10, 'max width 10').optional(),
 });
 
-schema
-  .validate({
-    firstName: 'asda',
-  })
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err.errors));
+type Person = z.infer<typeof schema>;
 
-try {
-  schema.validateSync({ firstName: 'asdd' });
-} catch (error) {
-  console.log(error.errors);
+const p1: Person = { firstName: '123' };
+
+const p2 = schema.safeParse({ firstName: 'asdas' });
+
+if (!p2.success) {
+  // console.log(p2.error.issues); // { "code": "too_small", "minimum": 5, "type": "string", "inclusive": true, "message": "at least 5", "path": ["firstName"]}
+  // console.log(p2.error.issues[0].message); // at least 5
+
+  console.log(p2.error.format()); // { _errors: [], firstName: { _errors: [ 'should be an email', 'at least 5' ] }}
+} else {
+  const data = p2.data;
+  console.log(data);
 }
