4cf2dbf1babfa11035234524c4618df893f0878f
Lei Zhao <lei.zhao@coupa.com>
2021-07-17 18:00:24 -0700

    add by auto checkIn

--- graphql_udemy/basics/src/server.ts
+++ graphql_udemy/basics/src/server.ts
@@ -5,14 +5,53 @@ export const startServer = async () => {
   // Construct a schema, using GraphQL schema language
   const typeDefs = gql`
     type Query {
-      hello: String!
+      greeting(name: String, position: String): User!
+      add(a: Float!, b: Float!): Float!
+      me: User!
+      post: Post!
+    }
+
+    type User {
+      id: ID!
+      name: String!
+      email: String!
+      age: Int
+    }
+
+    type Post {
+      id: ID!
+      title: String!
+      body: String!
+      published: Boolean!
     }
   `;
 
   // Provide resolver functions for your schema fields
   const resolvers = {
     Query: {
-      hello: () => 'Hello world!',
+      greeting: (parent, { name, position }, ctx, info) => {
+        // return `Hello, ${name}, your position is ${position}`;
+        return {
+          id: '123098',
+          name: 'Mike',
+          email: 'mike@example.com',
+        };
+      },
+      me: () => {
+        return {
+          id: '123098',
+          name: 'Mike',
+          email: 'mike@example.com',
+        };
+      },
+      post: () => {
+        return {
+          id: '092',
+          title: 'GraphQL 101',
+          body: '',
+          published: false,
+        };
+      },
     },
   };
 
