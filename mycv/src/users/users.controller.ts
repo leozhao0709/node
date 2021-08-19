c9e163752bf15a5690a5ea6b4426a7b23f55c9d5
Lei Zhao <lei.zhao@coupa.com>
2021-08-19 07:00:27 -0700

    add by auto checkIn

--- mycv/src/users/users.controller.ts
+++ mycv/src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { CurrentUser } from '../decorators/current-user.decorator';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('whomai')
+  whomai(@CurrentUser() currentUser: User) {
+    return currentUser;
+  }
+}
