551d30c3c635902f3aa14d4016c5e0a3ce6a3511
Lei Zhao <lei.zhao@coupa.com>
2021-08-18 18:00:26 -0700

    add by auto checkIn

--- mycv/src/index.ts
+++ mycv/src/index.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService, ConfigType } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
@@ -5,6 +6,12 @@ import appConfig from './config/app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    })
+  );
+
   const config = app
     .get(ConfigService)
     .get<ConfigType<typeof appConfig>>('app');
