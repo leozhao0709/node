6e4aad1f85f0fa7510ea4ae83eb0ad268690734e
Lei Zhao <lei.zhao@coupa.com>
2022-06-24 08:03:33 -0700

    add by auto checkIn

---
+++ decorator_play/jest.config.js
@@ -0,0 +1,18 @@
+const basicConfig = require('./jest.config.basic.cjs');
+
+module.exports = {
+  ...basicConfig,
+  preset: 'ts-jest/presets/default-esm',
+  globals: {
+    'ts-jest': {
+      // useESM: true,
+    },
+  },
+  setupFilesAfterEnv: ['./jest.setup.ts'],
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/__mocks__/fileMock.ts',
+    [`^@app/(.*)`]: '<rootDir>/src/$1',
+  },
+};
