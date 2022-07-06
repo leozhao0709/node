3adc093ac7c1b436036ea72dcdfbb2f4f2def4df
Lei Zhao <lei.zhao@coupa.com>
2022-07-05 18:00:21 -0700

    add by auto checkIn

---
+++ jest_play/packages/math/.lint-staged.config.js
@@ -0,0 +1,5 @@
+const baseConfig = require('../../config/common/lint-staged');
+
+module.exports = {
+  ...baseConfig,
+};
---
+++ jest_play/packages/math/src/index.ts
@@ -0,0 +1,1 @@
+export const sum = (...nums: number[]) => nums.reduce((res, num) => res + num);
